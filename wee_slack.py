c0ee37bbe89a99ed25868b526780a453c6d83199
Bryan Gilbert <gilbertw1@gmail.com>
2017-05-02 09:35:57 -0400

    added colorize lines config back

--- wee_slack.py
+++ wee_slack.py
@@ -3229,6 +3229,7 @@ class PluginConfig(object):
     # extracted.
     # TODO: setting descriptions.
     settings = {
+        'colorize_messages': 'false',
         'colorize_private_chats': 'false',
         'debug_mode': 'false',
         'debug_level': '3',
